5022acec173e12d941dafe91306c5a31cb757310
adry-an <adry.ann.dev@gmail.com>
2024-04-05 13:20:07 +0200

    add css 'nous-rencontrer' add media queries font-size

--- Planty/functions.php
+++ Planty/functions.php
@@ -7,6 +7,7 @@ function child_enqueue_styles()
 
     wp_enqueue_style('planty-theme-css', get_stylesheet_directory_uri() . '/css/style.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all');
     wp_enqueue_style('planty-header-footer-css', get_stylesheet_directory_uri() . '/css/header-footer.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all');
+    wp_enqueue_style('planty-nous-rencontrer-css', get_stylesheet_directory_uri() . '/css/nous-rencontrer-page.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all');
 }
 
 add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
