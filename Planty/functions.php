2ab9fca869c855315059e612c2492c47267fc882
adry-an <adry.ann.dev@gmail.com>
2024-04-30 15:53:59 +0200

    add home page (css + line 'enqueue style' php)

--- Planty/functions.php
+++ Planty/functions.php
@@ -9,6 +9,7 @@ function child_enqueue_styles()
     wp_enqueue_style('planty-header-footer-css', get_stylesheet_directory_uri() . '/css/header-footer.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all');
     wp_enqueue_style('planty-nous-rencontrer-css', get_stylesheet_directory_uri() . '/css/nous-rencontrer-page.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all');
     wp_enqueue_style('planty-commander-css', get_stylesheet_directory_uri() . '/css/commander-page.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all');
+    wp_enqueue_style('planty-accueil-css', get_stylesheet_directory_uri() . '/css/home-page.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all');
 }
 
 add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
